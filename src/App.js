fdeff983fa3559f25b7bdf2d74c769b4c2c24c7d
Hugh FENG Bi-Hui <hugh.feng79@TI03BY410.local>
2023-09-08 11:31:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Flex, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Flex
+        w="100%"
+        textAlign="center"
+        fontSize="xl"
+        justify="center"
+        pt="80px"
+      >
+        <Flex
+          w="200px"
+          border="1px solid"
+          borderColor="blue.500"
+          h="52px"
+          overflowX="scroll"
+          wrap="nowrap"
+        >
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red"></Flex>
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red"></Flex>
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red"></Flex>
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red"></Flex>
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red"></Flex>
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red"></Flex>
+          <Flex flexShrink="0" w="50px" h="50px" border="1px solid red">
+            last
+          </Flex>
+          <Flex flexShrink="0" w="10px" h="50px">
+            <Flex
+              w="50px"
+              h="50px"
+              ml="-40px"
+              bg="linear-gradient(270deg, #3A3A3A 0%, rgba(65, 65, 65, 0.00) 100%)"
+            ></Flex>
+          </Flex>
+        </Flex>
+      </Flex>
     </ChakraProvider>
   );
 }
